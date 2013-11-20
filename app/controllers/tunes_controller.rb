abae7281ce28b0607d10f576f93ca020f9cd9af9
Dave Tapley <dukedave@gmail.com>
2013-11-20 14:54:12 -0700

    Clean up

--- app/controllers/tunes_controller.rb
+++ app/controllers/tunes_controller.rb
@@ -9,9 +9,7 @@ class TunesController < ApplicationController
     @tune = Tune.find params[:id]
     match_notes = @tune.match_notes Player.notes
 
-    @matches = match_notes[:matches].collect do |m|
-      m
-    end
+    @matches = match_notes[:matches]
   end
 
 end
