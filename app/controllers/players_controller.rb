1c11d04178a005d51cef1d9c7f559c1c809aca96
Dave Tapley <dukedave@gmail.com>
2013-11-21 15:53:51 -0700

    Bug fix: Still need to allow :notes for create

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -20,7 +20,7 @@ class PlayersController < ApplicationController
   private
 
   def player_attributes
-    params.require(:player).permit(:name, :notes_string)
+    params.require(:player).permit(:name, :notes, :notes_string)
   end
 
 end
