aaf378fac9362922f4ac1fc2c8f8640999a2aa2f
Dave Tapley <dukedave@gmail.com>
2014-11-05 20:16:16 -0700

    #12 Link to and render best incomplete version

--- app/controllers/versions_controller.rb
+++ app/controllers/versions_controller.rb
@@ -2,9 +2,13 @@ class VersionsController < ApplicationController
 
   def show
     @tune = Tune.find params[:tune_id]
-    match_versions = @tune.match_notes(Player.notes).versions
 
-    @version = match_versions.at params[:id].to_i - 1
+    if params[:id] == 'best'
+      @version = @tune.match_notes(Player.notes).best_version
+    else
+      match_versions = @tune.match_notes(Player.notes).versions
+      @version = match_versions.at params[:id].to_i - 1
+    end
 
     raise ActiveRecord::RecordNotFound unless @version.present?
   end
